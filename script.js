d10f1042979e43a712ce5aa2f388d493bdad23e6
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-09-17 10:31:10 +0100

    I've connected to the giphy API

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const dropDownbtn = document.querySelector(".dropDownbtn");
+/*jshint esversion: 6 */
+const img = document.querySelector("img");
 
-dropDownbtn.addEventListener("click", () => {
-  const myDropDown = document.querySelector("#myDropDown");
-  myDropDown.classList.toggle("show");
-});
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=YN2xoWfsQ3CjewKOD7Tq0bXSpc1m8TB3&s=cats",
+  { mode: "cors" }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    link = response.data.images.original.url;
+    img.src = link;
+  });
